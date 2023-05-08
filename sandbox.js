a948339874f813be96e1de944360ac53649f6599
Dyes Jr <donjnr95@gmail.com>
2023-05-08 18:23:52 +0300

    Getting started with typescript

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+//ararys
+var names = ['luigi', 'dupont', 'Yatoshi', 56];
+//When you create an array the types can nort change later on
+var numbers = [10, 23, 56, 67];
+numbers.push(76);
+names.push('ted');
+names.push(45);
+var mixed = ['mkuru', 'chun-li', 67, 76, true];
+mixed.push(false);
+mixed.push(566);
+mixed[0] = 56;
+//Objects
+var mkuru = {
+    name: "mario",
+    belt: "black",
+    age: 34
+};
+mkuru.name = "Dyes";
+mkuru.age = 23;
+mkuru = {
+    name: "messi",
+    belt: "black",
+    age: 37,
+    // skills: "Programming"
+};
---
+++ sandbox.ts
@@ -0,0 +1,35 @@
+//ararys
+let names = ['luigi', 'dupont', 'Yatoshi', 56]
+//When you create an array the types can nort change later on
+
+
+let numbers = [10, 23, 56, 67]
+numbers.push(76)
+
+names.push('ted')
+names.push(45)
+
+let mixed = ['mkuru', 'chun-li', 67, 76, true]
+
+mixed.push(false)
+mixed.push(566)
+
+mixed[0] = 56
+
+
+//Objects
+let mkuru = {
+    name: "mario",
+    belt: "black",
+    age: 34
+}
+
+mkuru.name = "Dyes"
+mkuru.age = 23
+
+mkuru = {
+    name: "messi",
+    belt: "black",
+    age: 37,
+    // skills: "Programming"
+}
